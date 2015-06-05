203e9427aebe18fd1b553889edbf8a10380cfff1
Rene FGJr <renefgj@hotmail.com>
2015-06-05 10:02:46 -0300

    v0.15.23b
    -Configurações para o servidor

--- application/controllers/Banco_projetos.php
+++ application/controllers/Banco_projetos.php
@@ -1,5 +1,5 @@
 <?php
-class main extends CI_Controller {
+class banco_projetos extends CI_Controller {
 	function __construct() {
 		global $dd, $acao;
 		parent::__construct();
@@ -7,28 +7,34 @@ class main extends CI_Controller {
 		$this -> load -> library("nuSoap_lib");
 
 		$this -> load -> library('form_validation');
-		//$this -> load -> database();
+		$this -> load -> database();
 		$this -> load -> helper('form');
 		$this -> load -> helper('form_sisdoc');
 		$this -> load -> helper('url');
 		$this -> load -> library('session');
 		$this -> lang -> load("app", "portuguese");
 		date_default_timezone_set('America/Sao_Paulo');
-		
-		/* Security */
-		$this -> load -> model('login/josso_login_pucpr');
-		$this -> josso_login_pucpr -> security();
 
 		//$this -> lang -> load("app", "english");
 	}
+	
+	function security() {
+
+		/* Segurança */
+		$this -> load -> model('login/josso_login_pucpr');
+		$this -> josso_login_pucpr -> security();
+	}	
 
 	function index() {
+		/* Security */
+		$this->security();
 		
 		/* Carrega classes adicionais */
 		$css = array();
 		$js = array();
 		array_push($css, 'style_cab.css');
 		array_push($js, 'js_cab.js');
+		array_push($js, 'unslider.min.js');
 
 		/* transfere para variavel do codeigniter */
 		$data['css'] = $css;
@@ -37,18 +43,10 @@ class main extends CI_Controller {
 		/* Monta telas */
 		$this -> load -> view('header/header', $data);
 		$data['title_page'] = 'Menu Principal';
-		$data['menu'] = 0;
+		$data['menu'] = 1;
 		$this -> load -> view('header/cab', $data);
-
-		/* Menu */
-		$menu = array();
-		array_push($menu,array('CIP','Centro Integrado de Pesquisa, Administração','BTN','/cip'));
-		array_push($menu,array('Grupo de Pesquisa','Pesquisas da PUCPR','BTN','/dgp'));
-		array_push($menu,array('Banco de Projetos','Pesquisa realizadas na PUCPR','BTN','/banco_projetos'));
-		$data['menu'] = $menu;
-		$this -> load -> view('header/main_menu',$data);
 		
-		$this -> load -> view('header/foot');
+		$this -> load -> view('header/cab_image_slide', $data);
 	}
 
 }
---
+++ application/views/header/main_menu.php
@@ -0,0 +1,22 @@
+<div id="main_menu">
+<?php
+for ($r = 0; $r < count($menu); $r++) {
+	$tipo = $menu[$r][2];
+
+	switch ($tipo) {
+		case 'BTN' :
+			echo '
+				<a href="' . base_url('index.php'.$menu[$r][3]) . '\" onclick="parent.location=\'' . $menu[$r][3] . '\'" class="no-undeline">     
+					<div id="icone-cip-2" class="icone-iniciacao-cientifica icone-cip">
+						<h2 class="icone-iniciacao-cientifica-cor">' . $menu[$r][0] . '</h2>
+						<p>
+							' . $menu[$r][1] . '
+						</p>						
+					</div>
+				</a>
+			';
+			break;
+	}
+}
+?>
+</div>
