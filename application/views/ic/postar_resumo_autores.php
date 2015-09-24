75c5b9d95543aa4c3669af066be44716373e870f
Rene FGJr <renefgj@hotmail.com>
2015-09-24 18:00:49 -0300

    v0.15.38c

--- application/views/ic/postar_resumo_autores.php
+++ application/views/ic/postar_resumo_autores.php
@@ -6,8 +6,8 @@
 		<th width="10%">ação</th>
 	</tr>
 	<tr>
-		<td><input type="text" id="dd1" class="form_string" style="width: 100%;"></td>
-		<td><select id="dd2" class="form_string" style="width: 100%;" size=1>
+		<td><input type="text" id="dd10" name="dd10" class="form_string" style="width: 100%;"></td>
+		<td><select id="dd11" class="form_string" style="width: 100%;" size=1>
 			<option value=""></option>
 			<option value="2">Co-orientador</option>
 			<option value="3">Colaborador</option>
@@ -19,7 +19,7 @@
 			<option value="9">Orientador</option>
 			</select>
 		</td>
-		<td><input type="text" id="dd3" class="form_string" style="width: 100%;"></td>
+		<td><input type="text" id="dd12" class="form_string" style="width: 100%;"></td>
 		<td><input type="button" id="acao" class="form_button" style="width: 100%;" value="Incluir"></td>
 	</tr>
 </table>
@@ -29,9 +29,11 @@ $("#acao").click(function() {
 	$("#autores").html('Loading...');
 	var $url = '<?php echo base_url('index.php/ic/resumo_autores/' . $id . '/' . $check);?>';
 	var $dd10 = $("#dd10").val();
-	var $dd11 = $("#dd11").val();
+	var $dd11 = $("#dd11 option:selected").val();
 	var $dd12 = $("#dd12").val();
 	alert($dd10);
+	alert($dd11);
+	alert($dd12);
 	$.ajax({
 			url : $url,
 			type : "post",
