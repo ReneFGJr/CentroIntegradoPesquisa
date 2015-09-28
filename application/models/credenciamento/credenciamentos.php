6f6227f7b6ef9f668aa10ae9b8e2eef09b0f5c40
Rene FGJr <renefgj@gmail.com>
2015-09-27 21:38:32 -0300

    v.0.15.40

--- application/models/credenciamento/credenciamentos.php
+++ application/models/credenciamento/credenciamentos.php
@@ -85,11 +85,11 @@ class credenciamentos extends CI_Model {
 	}
 
 	function registro_form() {
-		$this->load->model("usuarios");
-		
+		$this -> load -> model("usuarios");
+
 		$id = sonumero($this -> input -> post("dd1"));
 		if (strlen($id) > 0) {
-			$id = $this->usuarios->limpa_cracha($id);
+			$id = $this -> usuarios -> limpa_cracha($id);
 			$this -> registra($id);
 			redirect(base_url('index.php/credenciamento/registro'));
 		}
@@ -108,6 +108,24 @@ class credenciamentos extends CI_Model {
 		return ($tela);
 	}
 
+	function entrega_kit_pessoa($id) {
+		$data = date("Y-m-d");
+		$hora = date("H:i:s");
+		$ip = $_SERVER['REMOTE_ADDR'];
+
+		$evento = round($_SESSION['evento_id']);
+
+		$sql = "insert into evento_kits 
+							(
+							kits_user_id, kits_data, kits_hora,
+							kits_ip, kits_evento
+							) values (
+							$id,'$data','$hora',
+							'$ip',$evento)
+					";
+		$this -> db -> query($sql);
+	}
+
 	function entrega_kit($cracha) {
 		$this -> load -> model("usuarios");
 		$cracha = $this -> usuarios -> limpa_cracha($cracha);
@@ -118,9 +136,12 @@ class credenciamentos extends CI_Model {
 			$id = 0;
 			if (isset($line['id_us'])) {
 				$id = $line['id_us'];
-				$this->nome = $line['us_nome'];
-				$this->cracha = $line['us_cracha'];
+				$this -> nome = $line['us_nome'];
+				$this -> cracha = $line['us_cracha'];
 			}
+
+			$edital = $this -> is_ic($cracha);
+			$mod = trim($edital['st_edital']);
 
 			if ($id > 0) {
 				$sql = "select * from evento_kits 
@@ -132,45 +153,34 @@ class credenciamentos extends CI_Model {
 					/* Já retirou */
 					return ('2');
 				} else {
-					$data = date("Y-m-d");
-					$hora = date("H:i:s");
-					$ip = $_SERVER['REMOTE_ADDR'];
-					
-					$evento = round($_SESSION['evento_id']);
-					
-					$sql = "insert into evento_kits 
-							(
-							kits_user_id, kits_data, kits_hora,
-							kits_ip, kits_evento
-							) values (
-							$id,'$data','$hora',
-							'$ip',$evento)
-					";
-					$this->db->query($sql);
+
 				}
 			}
 
-			$edital = $this -> is_ic($cracha);
 			if (count($edital) > 0) {
-				$mod = trim($edital['st_edital']);
 				if ($edital['st_professor'] == $cracha) {
+					$this->entrega_kit_pessoa($id);
 					return ('20');
 				}
-
 				if ($mod == 'PIBIC') {
+					$this->entrega_kit_pessoa($id);
 					return ('10');
 				}
 				/* PIBITI */
 				if ($mod == 'PIBITI') {
+					$this->entrega_kit_pessoa($id);
 					return ('11');
 				}
 				if ($mod == 'SENAI') {
+					$this->entrega_kit_pessoa($id);
 					return ('11');
 				}
 				if ($mod == 'JI') {
+					$this->entrega_kit_pessoa($id);
 					return ('12');
 				}
 				if ($mod == 'PIBICEM') {
+					$this->entrega_kit_pessoa($id);
 					return ('13');
 				}
 				return ('2');
@@ -184,12 +194,14 @@ class credenciamentos extends CI_Model {
 	function is_ic($cracha) {
 		$ano = (date("Y") - 1);
 		/* Verificar se é IC */
-		$sql = "select * from semic_nota_trabalhos 
-					where (st_professor = '$cracha' or st_aluno = '$cracha')
-							and (st_ano = '$ano') and (st_status <> 'C')							
-					 ";
-		//$sql .= " and (st_poster = 'S' or st_oral = 'S') ";
+		$sql = "select * from ic 
+					left join ic_aluno on id_ic = ic_id
+					left join semic_nota_trabalhos on ic_plano_aluno_codigo = st_codigo
+				where (ic_cracha_prof = '$cracha' or ic_cracha_aluno = '$cracha')
+						and ic_ano = '$ano' 
+				";
 
+		//$sql .= " and (st_poster = 'S' or st_oral = 'S') ";
 		$rlt = $this -> db -> query($sql);
 		$rlt = $rlt -> result_array();
 		if (count($rlt) > 0) {
@@ -201,14 +213,14 @@ class credenciamentos extends CI_Model {
 	}
 
 	function kits_form() {
-		$id = sonumero($this -> input -> post("dd1"));
+		$id = trim($this -> input -> post("dd1"));
 		$msg = '';
 		if (strlen($id) > 0) {
 			$rs = $this -> entrega_kit($id);
 			switch ($rs) {
 				case '2' :
 					$msg = '<font color="Green" class="lt6">Já Retirou o Kit!</font>';
-					break;				
+					break;
 				case '9' :
 					$msg = '<font color="red" class="lt6">Não tem trabalho para apresentar</font>';
 					break;
@@ -237,7 +249,7 @@ class credenciamentos extends CI_Model {
 					break;
 			}
 		}
-		$msg = '<center><font class="lt4">'.$this->nome.'</font><br>'.$msg;
+		$msg = '<center><font class="lt4">' . $this -> nome . '</font><br>' . $msg;
 		$form = new form;
 		$cp = array();
 		$cp[0] = array('$H8', '', '', False, False);
