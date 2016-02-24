878ffdb7af0872d79c6b61bf69abd5ac11b44e35
Elizandro Santos de Lima <santos.elizandro@pucpr.br>
2016-02-24 10:57:17 -0300

    #at.24.02.2016_a
    atualizacao

--- application/views/cnpq/semic_premiacao.php
+++ application/views/cnpq/semic_premiacao.php
@@ -27,23 +27,129 @@
 			<td align="center" valign="center"><h3>Fatos CN</h3></td>
 		</tr>
 		<tr>
-			<td align="right" width="250">Avaliador(1) </td>
+			<td align="left" width="250">Avaliador(1) </td>
 			<td class="lt4 tabela01" width="80" align="center">7,564</td>
 			<td class="lt5 tabela02" width="80" align="center">7,853</td>
 			<td class="lt4 tabela01" width="80" align="center">+0,289</td>
 		</tr>
 		<tr>
-			<td align="right" width="250">Avaliador(2) </td>
+			<td align="left" width="250">Avaliador(2) </td>
 			<td class="lt4 tabela01" width="80" align="center">8,5824</td>
 			<td class="lt5 tabela01" width="80" align="center">7,853</td>
 			<td class="lt4 tabela01" width="80" align="center">-0,729</td>
 		</tr>
 						<tr>
-			<td align="right" width="250">Avaliador(3) </td>
+			<td align="left" width="250">Avaliador(3) </td>
 			<td class="lt4 tabela01" width="80" align="center">9,582</td>
 			<td class="lt5 tabela01" width="80" align="center">7,853</td>
 			<td class="lt4 tabela01" width="80" align="center">-1,7299</td>
 		</tr>
-		
+	</table>
+	<br>
+	<br>
+	<br>
+	<table border=1 class="lt1">
+		<tr><br />
+			<h2>Notas de Planos e Relatórios</h2>
+		</tr>
+			<tr >
+			<td align="center" valign="center"><h3>Notas</h3></td>
+			<td align="center" valign="center"><h3>Redutor</h3></td>
+			<td align="center" valign="center"><h3>Ex.:</h3></td>
+			<td align="center" valign="center"><h3>Final com (Fatos CN)</h3></td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Submissão do Plano</td>
+			<td class="lt4 tabela01" width="80" align="center">70%</td>
+			<td class="lt5 tabela02" width="80" align="center">80,0</td>
+			<td class="lt4 tabela01" width="80" align="center">24,0</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Relatório Parcial </td>
+			<td class="lt4 tabela01" width="80" align="center">50%</td>
+			<td class="lt5 tabela01" width="80" align="center">75,0</td>
+			<td class="lt4 tabela01" width="80" align="center">37,5</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Relatório Final </td>
+			<td class="lt4 tabela01" width="80" align="center">0%</td>
+			<td class="lt5 tabela01" width="80" align="center">80,0</td>
+			<td class="lt4 tabela01" width="80" align="center">80,0</td>
+		</tr>
+	</table>
+	<br>
+	<br>
+	<br>
+	<table border=1 class="lt1">
+		<tr><br />
+			<h2>Notas das apresentações no SEMIC</h2>
+		</tr>
+			<tr >
+			<td align="center" valign="center"><h3>Notas</h3></td>
+			<td align="center" valign="center"><h3>Redutor</h3></td>
+			<td align="center" valign="center"><h3>Ex.:</h3></td>
+			<td align="center" valign="center"><h3>Final</h3></td>
+			<td align="center" valign="center"><h3>(SN)</h3></td>
+			<td align="center" valign="center"><h3>Nota Fiscal</h3></td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Apresentação Oral</td>
+			<td class="lt4 tabela01" width="80" align="center">0%</td>
+			<td class="lt5 tabela02" width="80" align="center">95,0</td>
+			<td class="lt4 tabela01" width="80" align="center">95,0</td>
+			<td class="lt4 tabela01" width="80" align="center">141,5</td>
+			<td class="lt4 tabela01" width="80" align="center">236,5</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Apresentação Pôster</td>
+			<td class="lt4 tabela01" width="80" align="center">0%</td>
+			<td class="lt5 tabela02" width="80" align="center">95,0</td>
+			<td class="lt4 tabela01" width="80" align="center">95,0</td>
+			<td class="lt4 tabela01" width="80" align="center">141,5</td>
+			<td class="lt4 tabela01" width="80" align="center">226,5</td>
+		</tr>
+	</table>
+	
+	<br />
+	<br />
+	<br />
+	<table border=1 class="lt1">
+		<tr><br />
+			<h2>PREMIAÇÃO DOS MELHORES TRABALHOS</h2>
+		</tr>
+			<tr>
+				<td style="border: 0">Premiação PIBIC</td>
+				<td style="border: 0" valign="bottom"><h2>7,853</h2></td>		
+			</tr>
+			<tr >
+			<td align="center" valign="center"><h3>Área</h3></td>
+			<td align="center" valign="center"><h3>Apresentação Oral</h3></td>
+			<td align="center" valign="center"><h3>Apresentação Pôster</h3></td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Ciências da Vida </td>
+			<td class="lt4 tabela01" width="80" align="center">7,564</td>
+			<td class="lt4 tabela01" width="80" align="center">+0,289</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Ciências Humanas, Linguistica, Letras e artes</td>
+			<td class="lt4 tabela01" width="80" align="center">1º Kit Congresso Nacional</td>
+			<td class="lt4 tabela01" width="80" align="center">2º Mensão Honrosa</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Ciências sociais Aplicadas</td>
+			<td class="lt4 tabela01" width="80" align="center">9,582</td>
+			<td class="lt5 tabela01" width="80" align="center">7,853</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Ciências Exatas e Engenharias</td>
+			<td class="lt4 tabela01" width="80" align="center">9,582</td>
+			<td class="lt5 tabela01" width="80" align="center">7,853</td>
+		</tr>
+		<tr>
+			<td align="left" width="250">Ciências Agrárias</td>
+			<td class="lt4 tabela01" width="80" align="center">9,582</td>
+			<td class="lt5 tabela01" width="80" align="center">7,853</td>
+		</tr>			
 	</table>
 	
