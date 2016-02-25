c58653c7e1bc9c6352af41b688166e9c2b13a556
Elizandro Santos de Lima <santos.elizandro@pucpr.br>
2016-02-25 14:07:09 -0300

    #at.25.02.2016.a
    At

--- application/views/cnpq/semic_premiacao.php
+++ application/views/cnpq/semic_premiacao.php
@@ -34,7 +34,7 @@
 		</tr>
 		<tr>
 			<td align="left" width="250">Avaliador(2) </td>
-			<td class="lt4 tabela01" width="80" align="center">8,5824</td>
+			<td class="lt4 tabela01" width="80" align="center">8,582</td>
 			<td class="lt5 tabela01" width="80" align="center">7,853</td>
 			<td class="lt4 tabela01" width="80" align="center">-0,729</td>
 		</tr>
@@ -42,7 +42,7 @@
 			<td align="left" width="250">Avaliador(3) </td>
 			<td class="lt4 tabela01" width="80" align="center">9,582</td>
 			<td class="lt5 tabela01" width="80" align="center">7,853</td>
-			<td class="lt4 tabela01" width="80" align="center">-1,7299</td>
+			<td class="lt4 tabela01" width="80" align="center">-1,729</td>
 		</tr>
 	</table>
 	<br>
@@ -74,7 +74,12 @@
 			<td align="left" width="250">Relatório Final </td>
 			<td class="lt4 tabela01" width="80" align="center">0%</td>
 			<td class="lt5 tabela01" width="80" align="center">80,0</td>
-			<td class="lt4 tabela01" width="80" align="center">80,0</td>
+			<td class="lt4 tabela0" width="80" align="center">80,0</td>
+		</tr>
+		<tr>
+			<td align="right" width="250" colspan="3">Soma das notas (SN) </td>
+			<td class="lt5 tabela01" align="right">141,5</td>
+
 		</tr>
 	</table>
 	<br>
@@ -109,47 +114,265 @@
 			<td class="lt4 tabela01" width="80" align="center">226,5</td>
 		</tr>
 	</table>
-	
 	<br />
 	<br />
 	<br />
-	<table border=1 class="lt1">
-		<tr><br />
-			<h2>PREMIAÇÃO DOS MELHORES TRABALHOS</h2>
-		</tr>
+	<table width="750" border="1">
+		<h2> Premiação PIBIC</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Apresentação Oral</h3></th>
+		    <th scope="col"><h3>Apresentação Pôster</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="3" align="left" width="250" style="border: none">Ciências da Vida</td>
+		    <td rowspan="2" class="lt2 tabela01" align="center" style="border: none">1º Kit Congresso Nacional</td>
+		    <td height="20" class="lt2 tabela01" align="center" style="border: none">1º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">3º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		  	<td style="border: none">&nbsp;</td>
+		  </tr>
+		  <tr>
+		    <td rowspan="3" align="left" width="250" style="border: none">Ciências Humanas, Linguística, Letras e Artes</td>
+		    <td rowspan="2" class="lt2 tabela01" align="center" style="border: none">1º Kit Congresso Nacional</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">3º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		  	<td style="border: none">&nbsp;</td>
+		  </tr>
+		  <tr>
+		    <td rowspan="3" align="left" width="250" style="border: none">Ciências Sociais Aplicadas</td>
+		    <td rowspan="2" class="lt2 tabela01" style="border: none">1º Kit Congresso Nacional</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">3º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		  	<td style="border: none">&nbsp;</td>
+		  </tr>
+		  <tr>
+		    <td rowspan="3" align="left" width="250" style="border: none">Ciências Exatas e Engenharias</td>
+		    <td rowspan="2" class="lt2 tabela01" align="center" style="border: none">1º Kit Congresso Nacional</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">3º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		  	<td style="border: none">&nbsp;</td>
+		  </tr>
 			<tr>
-				<td style="border: 0">Premiação PIBIC</td>
-				<td style="border: 0" valign="bottom"><h2>7,853</h2></td>		
+		    <td rowspan="3" align="left" width="250" style="border: none">Ciências Agrárias</td>
+		    <td rowspan="2" class="lt2 tabela01" style="border: none">1º Kit Congresso Nacional</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">3º Menção Honrosa</td>
+		  </tr>
+		  </table>
+		  <table>
+		  <tr style="border: none">
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>5</b> (valor médio R$ 8.000,00) Professores e estudantes</td>
 			</tr>
-			<tr >
-			<td align="center" valign="center"><h3>Área</h3></td>
-			<td align="center" valign="center"><h3>Apresentação Oral</h3></td>
-			<td align="center" valign="center"><h3>Apresentação Pôster</h3></td>
-		</tr>
-		<tr>
-			<td align="left" width="250">Ciências da Vida </td>
-			<td class="lt4 tabela01" width="80" align="center">7,564</td>
-			<td class="lt4 tabela01" width="80" align="center">+0,289</td>
-		</tr>
-		<tr>
-			<td align="left" width="250">Ciências Humanas, Linguistica, Letras e artes</td>
-			<td class="lt4 tabela01" width="80" align="center">1º Kit Congresso Nacional</td>
-			<td class="lt4 tabela01" width="80" align="center">2º Mensão Honrosa</td>
-		</tr>
-		<tr>
-			<td align="left" width="250">Ciências sociais Aplicadas</td>
-			<td class="lt4 tabela01" width="80" align="center">9,582</td>
-			<td class="lt5 tabela01" width="80" align="center">7,853</td>
-		</tr>
-		<tr>
-			<td align="left" width="250">Ciências Exatas e Engenharias</td>
-			<td class="lt4 tabela01" width="80" align="center">9,582</td>
-			<td class="lt5 tabela01" width="80" align="center">7,853</td>
-		</tr>
-		<tr>
-			<td align="left" width="250">Ciências Agrárias</td>
-			<td class="lt4 tabela01" width="80" align="center">9,582</td>
-			<td class="lt5 tabela01" width="80" align="center">7,853</td>
-		</tr>			
-	</table>
-	
+			<tr style="border: none">
+				<td align="right" width="250" style="border: none" class="lt2">Total de Menção Honrosa:  </td>
+				<td class="lt2" align="left" style="border: none" class="lt2"><b>20</b></td>
+			</tr>			  
+</table>
+	<br />
+	<br />
+	<br />
+<table width="750" border="1">
+		<h2> Premiação PIBITI</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Apresentação Oral</h3></th>
+		    <th scope="col"><h3>Apresentação Pôster</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="3" align="left" width="250" style="border: none">Todas as áreas</td>
+		    <td rowspan="2" class="lt2 tabela01" align="center" style="border: none">1º Kit Congresso Nacional</td>
+		    <td height="20" class="lt2 tabela01" align="center" style="border: none">1º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">&nbsp;</td>
+		  </tr>
+		  </table>
+		  <table>
+		  <tr >
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>1</b> (valor médio R$ 8.000,00) Professores e estudantes</td>
+			</tr>
+			<tr style="border: none">
+				<td align="right" width="250" style="border: none" class="lt2">Total de Menção Honrosa:  </td>
+				<td class="lt2" align="left" style="border: none" class="lt2"><b>3</b></td>
+			</tr>			  
+</table>
+	<br />
+	<br />
+	<br />
+<table width="750" border="1">
+		<h2> Premiação PIBIC Internacional</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Projeto</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="2" align="left" width="250" style="border: none">Todas as áreas</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º Kit Congresso Nacional</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+</table>
+<table>
+		  <tr >
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>1</b> (valor médio R$ 8.000,00) Professores e Orientador</td>
+			</tr>
+			<tr style="border: none">
+				<td align="right" width="250" style="border: none" class="lt2">Total de Menção Honrosa:  </td>
+				<td class="lt2" align="left" style="border: none" class="lt2"><b>1</b></td>
+			</tr>			  
+</table>
+	<br />
+	<br />
+	<br />
+<table width="750" border="1">
+		<h2> Premiação PIBIC_EM (jr)</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Oral/Pôster</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="2" align="left" width="250" style="border: none">Todas as áreas</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1 Tablet Samsung / ASUS</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">1 Tablet Samsung / ASUS</td>
+		  </tr>
+</table>
+<table>
+		  <tr >
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>2</b> (valor médio R$ 800,00) estudantes</td>
+			</tr>
+</table>
+	<br />
+	<br />
+	<br />
+<table width="750" border="1">
+		<h2> Premiação Jovens Idéias</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Projeto</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="2" align="left" width="250" style="border: none">Todas as áreas</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º R$ 1.000,00</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+</table>
+<table>
+		  <tr >
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>1</b> (valor médio R$ 1.000,00) Estudantes</td>
+			</tr>
+			<tr style="border: none">
+				<td align="right" width="250" style="border: none" class="lt2">Total de Menção Honrosa:  </td>
+				<td class="lt2" align="left" style="border: none" class="lt2"><b>1</b></td>
+			</tr>			  
+</table>
+	<br />
+	<br />
+	<br />
+<table width="750" border="1">
+		<h2> Premiação Pesquisar é Evoluir jr</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Projeto</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="2" align="left" width="250" style="border: none">Todas as áreas</td>
+		    <td class="lt2 tabela01" align="center" style="border: none">1º um vale livro de R$ 100,00</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º Menção Honrosa</td>
+		  </tr>
+</table>
+<table>
+		  <tr >
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>1</b> (valor médio R$ 100,00) Estudantes</td>
+			</tr>
+			<tr style="border: none">
+				<td align="right" width="250" style="border: none" class="lt2">Total de Menção Honrosa:  </td>
+				<td class="lt2" align="left" style="border: none" class="lt2"><b>1</b></td>
+			</tr>			  
+</table>	
+	<br />
+	<br />
+	<br />
+<table width="750" border="1">
+		<h2> Premiação Concurso "Eu <strike>não</strike> concordo com o Papa"</h2>
+		  <tr>
+		    <th scope="col"><h3>Área</h3></th>
+		    <th scope="col"><h3>Projeto</h3></th>
+		  </tr>
+		  <tr height="20">
+		    <td rowspan="3" align="left" width="250" style="border: none">Todas as áreas</td>
+		    
+		    
+		    <td class="lt2 tabela01" align="center" style="border: none">1º R$ 1.500,00</td>
+		  </tr>
+		  <tr>
+		    <td class="lt2 tabela01" align="center" style="border: none">2º R$ 1.000,00</td>
+		   </tr>
+		  <tr> 
+		    <td class="lt2 tabela01" align="center" style="border: none">3º R$ 500,00</td>
+		  </tr>
+
+</table>
+<table>
+		  <tr >
+				<td align="right" width="250" class="lt2" style="border: none">Total de Premiações:  </td>
+				<td class="lt2" align="left" style="border: none" colspan="2"><b>3</b> (valor médio R$ 3.000,00) trabalho</td>
+			</tr>
+</table>
+	<br />
+	<br />
+	<br />
