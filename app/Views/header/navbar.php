d2aa7fa83661050f66d7ff918cee00b3c4672a07
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-07-23 13:09:49 -0300

    v0.21.07.23

--- app/Views/header/navbar.php
+++ app/Views/header/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="<?php echo base_url(PATH);?>">Navbar</a>
+  <a class="navbar-brand" href="<?php echo base_url(PATH);?>">CIP</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
